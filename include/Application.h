1214958b442e6a0b40f379cf03c736e0e2378779
jave <java@itu.dk>
2026-02-25 20:14:42 -0300

    Added dear imgui to the project.

---
+++ include/Application.h
@@ -0,0 +1,15 @@
+#ifndef PQC_MASTER_THESIS_2026_APPLICATION_H
+#define PQC_MASTER_THESIS_2026_APPLICATION_H
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <functional>
+
+namespace Safira {
+    class Application {
+
+    };
+}
+
+#endif //PQC_MASTER_THESIS_2026_APPLICATION_H
---
+++ src/Application.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Jannik Verdoner on 25/02/2026.
+//
+
+#include "../include/Application.h"
+
+namespace Safira {
+} // Safira
